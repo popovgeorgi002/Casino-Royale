434146c3951c2e4a3e4a9a928422648daa7b61b5
popovgeorgi002 <popovgeorgi002@gmail.com>
2025-11-25 11:41:32 +0200

    Add routes

---
+++ user-service/src/routes/user.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { UserController } from '../controllers/user.controller';
+
+const router = Router();
+const userController = new UserController();
+
+router.post('/', userController.createUser);
+router.get('/:id', userController.getUserById);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+
+export default router;
